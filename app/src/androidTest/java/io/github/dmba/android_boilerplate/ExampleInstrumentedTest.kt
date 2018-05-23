d0c2b05f9666819a09fad196bf1ca20422b9575a
Dmytro Babiienko <dmitry.babienko@gmail.com>
2018-05-23 12:50:05 +0200

    Setup travis

--- app/src/androidTest/java/io/github/dmba/android_boilerplate/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/io/github/dmba/android_boilerplate/ExampleInstrumentedTest.kt
@@ -2,11 +2,9 @@ package io.github.dmba.android_boilerplate
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
-
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("io.github.dmba.android_boilerplate", appContext.packageName)
+        assertTrue(appContext.packageName.startsWith("io.github.dmba.android_boilerplate"))
     }
 }
